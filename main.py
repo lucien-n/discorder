cc0db93e6ac762f4124a33e766de804b8cc180ae
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-11-17 01:02:36 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,101 @@
+import requests
+import dotenv
+import os
+import time
+import argparse
+
+
+def get_arguments() -> dict[str, any]:
+    parser = argparse.ArgumentParser(description="Automatic message sender")
+
+    parser.add_argument(
+        "-r",
+        "--RoomId",
+        help="Room Id (right click on room or profile with discord developper mode ON)",
+    )
+    parser.add_argument(
+        "-m",
+        "--Message",
+        help="The message to send",
+    )
+    parser.add_argument("-l", "--Loop", help="Number of messages to send", type=int)
+    parser.add_argument(
+        "-d", "--Delay", help="Delay in seconds between each seocnd", type=int
+    )
+
+    args = parser.parse_args()
+
+    values = {}
+    if args.RoomId:
+        values["room_id"] = args.RoomId
+    if args.Message:
+        values["message"] = args.Message
+    if args.Loop:
+        values["loop"] = args.Loop
+    if args.Delay:
+        values["delay"] = args.Delay
+
+    return values
+
+
+def load_env(path: str) -> None:
+    if not dotenv.load_dotenv(path):
+        raise Exception(f"Could not load '{path}'")
+
+
+def get_input(prompt: str, wrapper, error: str = "Please enter a valid string") -> any:
+    try:
+        value = wrapper(input(prompt))
+    except ValueError:
+        print(error)
+        return get_input(prompt, error)
+    return value
+
+
+def send(token: str, url: str, payload: object, index: int) -> bool:
+    res = requests.post(url, data=payload, headers={"authorization": token})
+    status = res.status_code
+
+    if status != 200:
+        extra = ""
+        if status == 401:
+            extra = "Token might be invalid"
+
+        print(
+            f"[{status}] Failed to send message{f': {extra}' if extra else ''}",
+        )
+        return False
+
+    return True
+
+
+def main() -> None:
+    load_env(".env")
+    token = os.getenv("DISCORD_TOKEN")
+
+    args = get_arguments()
+
+    room_id = args["room_id"] if "room_id" in args else get_input("Room id >> ", str)
+    message = args["message"] if "message" in args else get_input("Message >> ", str)
+    loop = args["loop"] if "loop" in args else get_input("Loop >> ", int)
+    delay = args["delay"] if "delay" in args else get_input("Delay (s) >> ", int)
+
+    url = f"https://discord.com/api/v8/channels/{room_id}/messages"
+    payload = {"content": message}
+    failed_attempts = 0
+
+    for i in range(1, loop + 1):
+        time.sleep(delay)
+        print(f'Sending "{message}" №{i} of {loop}')
+
+        success = send(token, url, payload, i)
+
+        if not success:
+            failed_attempts += 1
+
+        if failed_attempts >= 3:
+            print("Reached max failed attempts (3), stopping...")
+            exit(1)
+
+
+main()
---
+++ readme.md
@@ -0,0 +1,37 @@
+# Discord message sender
+
+### Simple python script to mess with your friends <br/>
+### Use at your own risks as this script violates discord's TOS
+
+<br/>
+
+# Requirements
+  - python 3 (.11 preferably)
+  - a discord account
+  
+<br/>
+
+# How to
+1. Clone this repository
+2. Create a `.env` file
+3. Enter the following `DISCORD_TOKEN="<your discord token>"` [Help](#help)
+4. Install the requirements using `pip install -r requirements.txt`
+5. Run using `py ./main.py`
+
+<br/>
+
+# Arguments
+You can pass arguments in the command line instead of having to fill out prompts
+ - *-r* the room id
+ - *-m* the message to send
+ - *-l* the number of times you want to send this message
+ - *-d* the delay between each messages
+
+<br/>
+
+# Help
+[How to get my discord token ?](https://www.youtube.com/watch?v=YEgFvgg7ZPI) **BE VERY CAREFUL WITH THIS TOKEN - DO NOT SHARE IT WITH ANYONE**
+
+<br/>
+
+### PS: This was just made for fun, don't be a jerk  :D
